0016fcbaa6c1a21db844b5d677140e8534813f2d
Mark <marklin1106@gmail.com>
2021-08-24 23:28:17 +0800

    doc: Add example

---
+++ examples/ExampleDefault/FormContent.jsx
@@ -0,0 +1,31 @@
+/* eslint no-console: 0 */
+
+import React from 'react';
+import { useFormikContext } from 'formik';
+
+import FormikTokenInput from '../../src';
+
+const handlePreprocess = (inputValues) => {
+  // Remove blanks from input strings' header and tail
+  return inputValues.map((inputValue) => inputValue.trim());
+};
+
+function FormContent() {
+  const formikContext = useFormikContext();
+
+  console.log('formikContext', formikContext);
+
+  const { dirty, isValid, isSubmitting, submitForm } = formikContext;
+  const disableSubmitButton = !dirty || !isValid || isSubmitting;
+
+  return (
+    <>
+      <FormikTokenInput name="emails" onPreprocess={handlePreprocess} />
+      <button disabled={disableSubmitButton} onClick={submitForm}>
+        Submit
+      </button>
+    </>
+  );
+}
+
+export default FormContent;
