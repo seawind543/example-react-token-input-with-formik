fed77a83c94c7ea1ec3d1ff0fda971ea7a50fc6f
Mark <marklin1106@gmail.com>
2023-01-24 15:13:27 +0800

    fix: lint issue

--- examples/index.tsx
+++ examples/index.tsx
@@ -73,5 +73,6 @@ const App = () => {
 };
 
 const container = document.getElementById('container');
+/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<App />);
