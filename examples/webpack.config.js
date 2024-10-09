5668be714e78078a7401d8c3f8f413b02955fd15
Mark <marklin1106@gmail.com>
2024-10-09 17:16:22 +0800

    build: apply webpack clean function instead of rm

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../docs'),
     filename: 'bundle.js?[fullhash]',
+    clean: true, // Clean the output directory before emit.
   },
   optimization: {
     moduleIds: 'named',
